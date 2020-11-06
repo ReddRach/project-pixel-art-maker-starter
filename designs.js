36d7842697fda664b8e6b3ecb9fac5c0209c0d85
ReddRach <71412913+ReddRach@users.noreply.github.com>
2020-11-06 02:49:32 -0600

    feat: assigned constant variables definitions
    constant variables are referenced from html.index

--- designs.js
+++ designs.js
@@ -1,8 +1,10 @@
 // Select color input
+const color=document.querySelector("#colorPicker");
 // Select size input
+const size= document.querySelector("sizePicker");
+const table=document.querySelector("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
-
 function makeGrid() {
 
 // Your code goes here!
