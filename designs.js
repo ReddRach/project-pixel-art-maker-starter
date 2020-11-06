983a1b14a307d47d2f38ff5dd4bb1b1aa378bbda
ReddRach <71412913+ReddRach@users.noreply.github.com>
2020-11-06 03:00:31 -0600

    feat: assigned variables for table dimensions
    8- reset table after hitting submit button
    14- started on code to select table dimensions

--- designs.js
+++ designs.js
@@ -5,8 +5,16 @@ const size= document.querySelector("sizePicker");
 const table=document.querySelector("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
-function makeGrid() {
+size.addEventListener('submit', function(e) {
+  e.preventDefault();
+  makeGrid()
+});
 
+//makeGrid()
+function makeGrid() {
+  table.innerHTML="";
+  let height=document.querySelector('#inputHeight").value;
+  let width=document.querySelector('#inputWidth").value;
 // Your code goes here!
 
 }
